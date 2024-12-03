8800f659a9df28f339ac4ac966ae590c93816631
puntacode <pablo.molina@gmail.com>
2024-12-03 08:50:47 -0300

    1er commit base de WebApi con Net6 y Swagger

---
+++ Controllers/EstudianteController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EstudianteController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "producto1", "producto2" };
+        }
+    }
+}
